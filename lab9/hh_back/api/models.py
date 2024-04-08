5f431b689544b500094302f2667c378efa48139b
Dimash Amalbek <d_amalbek@kbtu.kz>
2024-04-08 15:08:31 +0500

    lab

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -6,8 +6,15 @@ class Company(models.Model):
     city = models.CharField(max_length=50)
     address = models.TextField()
 
-    def __str__(self):
-        return self.name
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+    
     
 class Vacancy(models.Model):
     name = models.CharField(max_length=100)
@@ -15,5 +22,11 @@ class Vacancy(models.Model):
     salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company.name
+        }
